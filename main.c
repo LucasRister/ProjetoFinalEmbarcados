5f9e0a9ab96b52b2163ecc1389b715cc548c6bdd
Lucas Rister <lucas_rm1@live.com>
2021-08-20 20:33:51 -0300

    process and show function

--- main.c
+++ main.c
@@ -69,3 +69,75 @@ int state;
       }   
   }
 }
+
+static void process1(void *pvParameters) // Task para executar as funcoes referente a cada btn
+{
+
+  unsigned long delayBotao; //Delay do debounce
+  int estadoBotaoAnt = 0; //Var temp para o debounce
+  int32_t remainder =0; //var para o resto da divisao do conversor de bin para dec
+  int32_t base =1; // var para a base multiplicativa do conversor de bin para dec
+  int state1; //var que recebe qual btn foi apertado
+  int input =0; //var que determina o que mostrar no display 0 - entrada 1 - conversao
+  
+  for(;;){
+      xQueueReceive(queue, &state1, portMAX_DELAY);
+      if ((millis() - delayBotao) > tempoDebounce) {
+         if ((state1 != 0) && (estadoBotaoAnt == 0) ) {        
+              if(state1 == 2){ //se btn RIGHT pressionado 
+                result = result*10 + 1; //recebe 1 e pula para o proximo digito
+                input = 0; // Seta o display para mostrar os dados de entrada
+                xQueueSend(queue2, &input, portMAX_DELAY);
+              }
+              if(state1 == 5){ // se btn LEFT pressionado
+                result = result*10; // recebe 0 e pula para o proximo digito
+                input = 0;  //Seta o display para mostrar os dados de entrada
+                xQueueSend(queue2, &input, portMAX_DELAY);
+              }
+              if(state1 == 3){//se btn UP pressionado convert Bin to Dec
+                remainder =0;
+                decimal = 0;
+                base =1;
+                while(result > 0){
+                  remainder =result % 10;
+                  decimal = decimal + remainder* base;
+                  result = result/10;
+                  base = base*2;
+                }   
+                input = 1; // seta o display para mostrar a conversao de bin para dec
+                xQueueSend(queue2, &input, portMAX_DELAY);
+              }     
+            delayBotao = millis();
+         }
+         if ((state1 == 0) && (estadoBotaoAnt != 0) ) {
+            delayBotao = millis();
+         }
+      }
+      estadoBotaoAnt = state1;
+      input =0;    // Seta o display para mostrar os dados de entrada
+      xQueueSend(queue2, &input, portMAX_DELAY); 
+  }
+}
+
+static void Show1(void *pvParameters){ // Task para mostrar os dados na tela
+
+  int input1 =0;
+  
+  for(;;){
+    xQueueReceive(queue2, &input1, portMAX_DELAY);
+    if(input1 ==0 ){ //Condicao para mostrar os dados da entrada
+        lcd.setCursor(0,0);
+        lcd.print("Input:");
+        lcd.setCursor(2,1);
+        lcd.print(result); //Mostra os dados da entrada
+    }else if(input1 ==1){//Condicao para mostrar os dados convertidos
+        lcd.clear();
+        lcd.print("Dec:");
+        lcd.setCursor(2,1);
+        lcd.print(decimal); //Mostra a conversao
+        vTaskDelay(pdMS_TO_TICKS(5000)); //Deixa os dados na tela por 5s
+        result =0;  //Reseta a var dos dados convertidos
+        lcd.clear(); //Reseta o display
+    }
+  }
+}
